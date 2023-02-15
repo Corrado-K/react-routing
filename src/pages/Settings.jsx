5e96493aef508bb50c62909dae05d95d115f3af9
Corrado-K <kebemartey@gmail.com>
2023-02-15 14:29:36 +0000

    Update settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,7 +2,6 @@ const Settings = () => {
      return ( 
           <div className="h-full px-20 py-10">
                <h2 className="text-lg font-semibold">Settings</h2>
-
           </div>
       );
 }
